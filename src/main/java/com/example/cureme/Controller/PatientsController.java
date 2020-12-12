aca34543db132a51fd00a52e1448e29bf352dd37
XinLvlv <942820966@qq.com>
2020-12-12 21:07:59 +0800

    Send message

--- src/main/java/com/example/cureme/Controller/PatientsController.java
+++ src/main/java/com/example/cureme/Controller/PatientsController.java
@@ -1,6 +1,7 @@
 package com.example.cureme.Controller;
 import com.example.cureme.Entity.Patients;
 import com.example.cureme.Service.PatientsService;
+import com.example.cureme.Utility.SendMessageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.relational.core.sql.In;
 import org.springframework.stereotype.Controller;
@@ -29,6 +30,8 @@ public class PatientsController {
         Patients patient = patientsService.add(FirstName, LastName, Address, PhoneNumber, Mail, Disease,Age,
                 sdf.parse(DOB), Gender);
         model.addAttribute("patient", patient);
+        int resultCode = SendMessageUtil.send("xlll","d41d8cd98f00b204e980",PhoneNumber,"Welcome to CareMe. Here we help to secure your health. username:"+patient.getFirstName()+"; password:"+patient.getPassword());
+        System.out.println(SendMessageUtil.getMessage(resultCode));
         return "patientInfo";
     }
 }
